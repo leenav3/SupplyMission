c853588c82fcaea3c047acf501558399406b4621
leenav3 <65767815+leenav3@users.noreply.github.com>
2021-01-03 15:02:11 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,84 @@
-var helicopterIMG, helicopter, package,packageIMG;
-var ground,box;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody = [];
+var ground;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
+var boxbody1,side1;
 function preload()
 {
 	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	//packageIMG=loadImage("package.png")
 }
 
 function setup() {
 	createCanvas(800, 700);
-	rectMode(CENTER);
-	engine=Engine.create();
-	world=engine.world;
-	package = Bodies.rectangle(width/2,100,20,20,{restitution:0.5,isStatic:true});
-	World.add(world,package);
-	ground=Bodies.rectangle(width/2,height,width,20,{isStatic:true})
-	World.add(world,ground)
 
-	box = new Box(width/2,height-20,200,20);
+	engine = Engine.create();
+	world = engine.world;
+	//rectMode(CENTER);
+	
+	
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	boxPosition=width/2-100
+	boxY=610;
+	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+40, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+	  
 	Engine.run(engine);
+	
+    textSize(20)
 }
+
+
+function keyPressed() {
+	//Engine.update(engine);
+ if (keyCode === DOWN_ARROW || keyCode === 32) {
+    packageBody.push(new Package(width/2, 200 , 20 , 20));
+	
+  }
+}
+
 
 function draw() {
-  background(0);
-  imageMode (CENTER)
-  box.display();
- image(packageIMG,package.position.x,package.position.y,20,20)
- image(helicopterIMG, width/2, 100,200,100); 
- 
- rect(ground.position.x,ground.position.y,width,20);
+  
+  background("yellow");
+  text("Press down arrow key to release more packages",50,50)
+  rectMode(CENTER);
+  for(var i=0;i< packageBody.length;i++){
+ 	 packageBody[i].display();
+  }  
+  drawSprites();
  
 }
 
-function keyPressed(){
-	console.log(keyCode)
-	if(keyCode === 32){
-		Matter.Body.setStatic(package,false);
-	
-	}
-	
-}
+
+
+
